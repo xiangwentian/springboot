6a87cddf1bd6b0217b8b921ffbd20262a28428c3
liuzhuangjob@163.com <liuzhuang5977>
2019-12-29 21:48:48 +0800

    springboot test

---
+++ springboot/src/test/java/com/workman/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workman.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
